38ff49f9ef627cdabe4fafb535f56eda3c3c503e
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2023-05-13 23:34:24 +0700

    push

---
+++ New folder (2)/main.js
@@ -0,0 +1,12 @@
+function show(){
+    var a = document.getElementById('hiennut');
+ 
+    a.style.display = 'block';
+   
+}
+
+function tatnut(){
+    var a = document.getElementById('hiennut');
+
+    a.style.display = 'none';
+}
