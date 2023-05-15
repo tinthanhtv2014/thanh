d9b98f2ff7fc4b57f709ad7434922bc8856ec1cb
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2023-05-15 21:14:24 +0700

    push

--- New folder (2)/main.js
+++ New folder (2)/main.js
@@ -11,10 +11,24 @@ function tatnut(){
     a.style.display = 'none';
 }
 
-var tendangnhap = document.getElementById('name1').value;
-var email = document.getElementById('sdt').value;
+
+
+
+
+
+var e = document.getElementById('abcc');
+e.style.backgroundColor = 'red';
+
 
 function sub(){
+   var c = document.name1.name2.value;
+   var b = document.name3.name4.value;
+   if(b.length > 0){
     var a = document.getElementById('hiennut');
     a.style.display = 'none';
+    alert('cảm ơn: '+ c + ' số điện thoại '+b);
+   }else{
+    alert('vui lòng nhập lại');
+   }
+    
 }
