dbe09b0ef8fa951cd779e0cacb76ce9bfb8911f9
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2023-05-15 11:46:29 +0700

    push

--- New folder (2)/main.js
+++ New folder (2)/main.js
@@ -10,3 +10,11 @@ function tatnut(){
 
     a.style.display = 'none';
 }
+
+var tendangnhap = document.getElementById('name1').value;
+var email = document.getElementById('sdt').value;
+
+function sub(){
+    var a = document.getElementById('hiennut');
+    a.style.display = 'none';
+}
