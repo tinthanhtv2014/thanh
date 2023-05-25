4ff586f6e19f77b608c8be6207020c38788e56e1
tinthanhtv2014 <130839620+tinthanhtv2014@users.noreply.github.com>
2023-05-25 22:45:41 +0700

    push

--- New folder (2)/main.js
+++ New folder (2)/main.js
@@ -1,3 +1,9 @@
+
+
+setInterval(function() {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.querySelector('h1').style.color = "#" + randomColor;
+  }, 1000);
 function show(){
     var a = document.getElementById('hiennut');
  
